1b6d01c33ecdba6bdfffef022242a309bb6401d2
mona-almalki <mona.fawazalmalki76@gmail.com>
2025-09-13 17:11:53 +0300

    Add Contact Me Section & some updates on Css

--- Portfolio/scripts/main.js
+++ Portfolio/scripts/main.js
@@ -8,11 +8,46 @@ function init() {
   });
 
   // I implemented scroll reveal animation using scrollreveal API: https://scrollrevealjs.org/api/reveal.html
-  ScrollReveal().reveal('.ScrollReveal', {
-    delay:300,
-    duration:600,
-    origin:'top',  
-    reset: true          
+  ScrollReveal().reveal(".ScrollReveal", {
+    delay: 300,
+    duration: 600,
+    origin: "top",
+    reset: true,
   });
 }
 document.addEventListener("DOMContentLoaded", init);
+
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form";
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+    });
+}
+form.addEventListener("submit", handleSubmit);
